00f9237fd94f2f31c30dea37a58b7ae3ba0326da
Rene Neubert <rene.neubert@aldeso.com>
2018-02-17 00:48:43 +0100

    fix(grgit):

---
+++ lemon-core/src/integTest/java/org/softcake/lemon/core/integtest/PrivateConstructorTesterParametersIntegTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2018 softcake.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.softcake.lemon.core.integtest;
+
+import org.softcake.lemon.core.tester.PrivateConstructorTester;
+import org.softcake.lemon.core.tester.testclasses.defaults.DefaultParameterPrivate;
+import org.softcake.lemon.core.tester.testclasses.nonstatics.NonStaticParameter;
+import org.softcake.lemon.core.tester.utils.TestUtil;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * PrivateConstructorTesterTest Class.
+ *
+ * @author Rene Neubert @softcake.org
+ */
+@SuppressWarnings("all")
+public class PrivateConstructorTesterParametersIntegTest {
+
+    private static final String MISMATCH_IN = "Mismatch in ";
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void expectedWithNullParameters_defaultWithParameter_assert() {
+
+        Class<DefaultParameterPrivate> aClass = DefaultParameterPrivate.class;
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Parameter [expectedParameters] must not be null or empty!");
+        PrivateConstructorTester.forClass(aClass)
+                                .expectedWithParameters()
+                                .check();
+
+    }
+
+    @Test
+    public void expectedWithNullParameters_defaultWithParameterNotMatch_throw() {
+
+        Class<DefaultParameterPrivate> aClass = DefaultParameterPrivate.class;
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Parameter [expectedParameters] must not be null or empty!");
+
+        PrivateConstructorTester.forClass(aClass).expectedWithParameters().check();
+    }
+
+    @Test
+    public void expectedWithNullParameters_nonstaticWithParameter_assert() {
+
+        String classShortName = "NonStaticPrivate";
+        Class<NonStaticParameter> aClass = NonStaticParameter.class;
+
+        Object enclosingInstance = TestUtil.getEnclosingInstance(aClass,
+                                                                 new Object[]{},
+                                                                 new Class<?>[]{});
+        Class<?> nestedClass = TestUtil.forNestedName(classShortName, enclosingInstance);
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Parameter [expectedParameters] must not be null or empty!");
+        PrivateConstructorTester.forClass(nestedClass, enclosingInstance).expectedWithParameters().check();
+    }
+
+    @Test
+    public void expectedWithNullParameters_nonstaticWithParameterNotMatch_throw() {
+
+        String classShortName = "NonStaticPrivate";
+        Class<NonStaticParameter> aClass = NonStaticParameter.class;
+
+        Object enclosingInstance = TestUtil.getEnclosingInstance(aClass,
+                                                                 new Object[]{},
+                                                                 new Class<?>[]{});
+        Class<?> nestedClass = TestUtil.forNestedName(classShortName, enclosingInstance);
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Parameter [expectedParameters] must not be null or empty!");
+        PrivateConstructorTester.forClass(nestedClass, enclosingInstance)
+                                .expectedWithParameters()
+                                .check();
+    }
+
+    @Test
+    public void forClass_defaultWithNullParameterNotMatch_throw() {
+
+        Class<DefaultParameterPrivate> aClass = DefaultParameterPrivate.class;
+        thrown.expect(AssertionError.class);
+        thrown.expectMessage(aClass.toString()
+                             + " has non-default constructor with some parameters");
+
+        PrivateConstructorTester.forClass(aClass).check();
+    }
+
+    @Test
+    public void forClass__nonstaticWithNullParameterNotMatch_throw() {
+
+        String classShortName = "NonStaticPrivate";
+        Class<NonStaticParameter> aClass = NonStaticParameter.class;
+
+        Object enclosingInstance = TestUtil.getEnclosingInstance(aClass,
+                                                                 new Object[]{},
+                                                                 new Class<?>[]{});
+        Class<?> nestedClass = TestUtil.forNestedName(classShortName, enclosingInstance);
+        thrown.expect(AssertionError.class);
+        thrown.expectMessage(aClass.toString()
+                             + "$"
+                             + classShortName
+                             + " has non-default constructor with some parameters");
+        PrivateConstructorTester.forClass(nestedClass, enclosingInstance).check();
+    }
+}
+
